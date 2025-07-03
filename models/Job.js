57e3587782b07da1fddb24c25230c772b60835a2
raviSobwani76656 <sobwaniravi@gmail.com>
2025-07-03 20:00:32 +0530

    Update Job.js

--- models/Job.js
+++ models/Job.js
@@ -1,37 +1,4 @@
-// const { DataTypes } = require("sequelize");
-// const sequelize = require("../config/database");
 
-
-// const Job = sequelize.define("Job", {
-
-//     // Jobid: {
-
-//     //     type: DataTypes.INTEGER,
-//     //     autoIncreament: true,
-//     //     primaryKey: true,
-
-//     // },
-
-//     title: {
-//         type: DataTypes.STRING,
-//         allowNull: false
-//     },
-
-//     description: {
-
-//         type: DataTypes.STRING,
-//         allowNull: false
-//     },
-
-//     slug: {
-//         type: DataTypes.STRING,
-//         allowNull: false
-
-//     }
-// })
-
-
-// module.exports = { Job };
 
 const { DataTypes } = require("sequelize");
 
